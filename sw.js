20780de62e584c5f32b25f3ad0bc85d3c58e1826
housamo-prog <housamo@gmail.com>
2026-09-25 01:30:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'erp-accounting-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(names => Promise.all(
+            names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+        ))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    if (e.request.url.includes('cdn.jsdelivr.net')) return;
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request).then(response => {
+                if (e.request.method === 'GET' && response.status === 200) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+                }
+                return response;
+            });
+        }).catch(() => caches.match('./index.html'))
+    );
+});
